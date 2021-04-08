3dadd169a1a85523832479bb8590b72a78babf97
Bach Nghia Nguyen <bachnghianguyen@gmail.com>
2021-04-08 17:06:27 +0700

    update README.md, create issue modal

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+import IssueModal from "./components/IssueModal";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1>Hoang</h1>
-      </header>
-    </div>
+    <>
+      <IssueModal issue={""} />
+    </>
   );
-}
+};
 
 export default App;
